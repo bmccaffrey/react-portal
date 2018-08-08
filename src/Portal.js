72c18cf72db962f9287be2f67c78edcc368c0bad
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-08 08:37:18 -0500

    add constructor

--- src/Portal.js
+++ src/Portal.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 const portalRoot = document.getElementById('portal');
 
 export default class Portal extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     const { children } = this.props;
     return ReactDOM.createPortal(children);
