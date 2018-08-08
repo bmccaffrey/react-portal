5edf99a913021dd15e5374fc8ba1bcb2e578aef0
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-08 08:40:54 -0500

    remove element on will unmount

--- src/Portal.js
+++ src/Portal.js
@@ -12,6 +12,10 @@ export default class Portal extends Component {
     portalRoot.appendChild(this.el);
   };
 
+  componentWillUnmount = () => {
+    portalRoot.removeChild(this.el);
+  };
+
   render() {
     const { children } = this.props;
     return ReactDOM.createPortal(children, this.el);
