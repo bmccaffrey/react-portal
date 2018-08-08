a25955b6033838f708e2cc9875d0da319ca8af4a
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-08 08:33:29 -0500

    store portal render target in constant

--- src/Portal.js
+++ src/Portal.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import ReactDOM from 'react-dom';
+const portalRoot = document.getElementById('portal');
 
 export default class Portal extends Component {
   render() {
